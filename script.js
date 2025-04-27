dc532a3a150700ca94f8d88e5ad34d7f1f84dbaa
Abhishek Rathod <118195093+abhishek-rathod1@users.noreply.github.com>
2025-04-27 13:46:14 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,50 @@
 //your JS code here. If required.
+const op = document.getElementById("output");
+
+
+myPromise1.then(()=>{
+	// op.textContent = 
+	return myPromise2;
+}).then(data2 =>{
+	op.textContent = data2;
+	return myPromise3;
+}).then(data3=>{
+	op.textContent = data3;
+}).catch((err)=>{
+	console.log(err);
+})
+
+
+
+function myPromise1() {
+	return new Promise((resolve, reject)=>{
+		setTimeout(()=>{
+			resolve([1,2,3,4])
+		},3000)
+	})	
+}
+
+function myPromise2(arr) {
+	return new Promise((reslove, reject)=>{
+		setTimeout(()=>{
+			reslove([arr.filter((i)=>i%2==0)])
+		},1000)
+	})	
+}
+
+function myPromise3() {
+	return new Promise((resolve, reject)=>{
+		setTimeout(()=>{
+			resolve([4,8])
+		},1000)
+	})
+	
+}
+
+
+
+
+
+
+
+
